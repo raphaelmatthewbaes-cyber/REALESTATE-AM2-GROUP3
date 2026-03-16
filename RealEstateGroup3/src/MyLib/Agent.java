22eaa746489aad8916ca4b87c345ea5c27c9c629
raphaelmatthewbaes-cyber <raphaelmatthew.baes@gmail.com>
2026-03-16 18:14:19 +0800

    Update Agent.java

--- RealEstateGroup3/src/MyLib/Agent.java
+++ RealEstateGroup3/src/MyLib/Agent.java
@@ -19,19 +19,39 @@ public class Agent extends User{
     }
     
     public void manageProperty() {
-        
+        System.out.println("Agent is managing assigned properties:");
+
+        if (assignedProperties == null) {
+            System.out.println("No properties assigned.");
+            return;
+        }
+
+        for (Property p : assignedProperties) {
+            if (p != null) {
+                System.out.println("Property located at: " + p.getLocation());
+            }
+        }
     }
     
     public void scheduleViewing(Property property, Buyer buyer) {
         //perhaps we can set specific days of the week and hours
+        System.out.println("Viewing scheduled.");
+        System.out.println("Buyer: " + buyer.getName());
+        System.out.println("Property location: " + property.getLocation());
     }
     
     public void negotiatePrice() {
         //im aSSuming this is will input by agent as a secondary actor
+        double price = property.getPrice();
+        double negotiatedPrice = price * 0.95;
+
+        System.out.println("Original price: " + price);
+        System.out.println("Negotiated price: " + negotiatedPrice);
     }
     
     public void getReport(Report report) {
         //will there be a format for this
+        return propertyPrice * commissionRate;
     }
     
 }
